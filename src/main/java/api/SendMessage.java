d7654db5f3179a76218870fcde38e3ecb4d61bb0
AndresFelipeRinconGamboa <andres.rincon@mail.mcgill.ca>
2017-02-11 09:23:56 -0500

    Adding code to sendMessage
    I added some code. 
    We need to create a gmail account for our application. It will make things easier

--- src/main/java/api/SendMessage.java
+++ src/main/java/api/SendMessage.java
@@ -1,37 +1,82 @@
 package api;
 
-
-
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import template.Constant;
+import template.User.User;
+import template.User.UserDao;
 import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
 import javax.activation.*;
-
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import java.util.List;
 
 public class SendMessage(){
 
-    private String from = "";
+    private String from = "booktraders@gmail.com";
+    private String password = "booktraders1";
     private String to = "";
     private String content = "";
     private String host = "localhost";
+    private UserDao userDao;
 
+    
+   @Path("/send_email/{user_id}{sender_email}{content}")
+   public  void send_email(@PathParam("user_id") String userId, @PathParam("sender_email") String senderEmail, @PathParam("content")String content) {
+        String from = this.from;
+        String pass = this.password;
+        String[] to = {userDao.getEmailByID(user_id)}; // list of recipient email addresses
+        String subject = "Email from Book traders application";
+        String body = "You received a message from the application Book traders. FROM: " + senderEmail +"\n"+content;
 
-    public SendMessage(String receiver_email, String sender_email, String text){
-
-
-
-                this.from = sender_email;
-                this.to = receiver_email;
-                this.content = text;
-
-
+        sendFromGMail(from, pass, to, subject, body);
     }
-    public void send_msg(){
-            
+    //Source: http://stackoverflow.com/questions/46663/how-can-i-send-an-email-by-java-application-using-gmail-yahoo-or-hotmail
+    private void sendFromGMail(String from, String pass, String[] to, String subject, String body) {
+        Properties props = System.getProperties();
+        String host = "smtp.gmail.com";
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", from);
+        props.put("mail.smtp.password", pass);
+        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.auth", "true");
 
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(from));
+            InternetAddress[] toAddress = new InternetAddress[to.length];
+
+            // To get the array of addresses
+            for( int i = 0; i < to.length; i++ ) {
+                toAddress[i] = new InternetAddress(to[i]);
+            }
+
+            for( int i = 0; i < toAddress.length; i++) {
+                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+            }
+
+            message.setSubject(subject);
+            message.setText(body);
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, pass);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (AddressException ae) {
+            ae.printStackTrace();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();
+        }
     }
-
-
 
 
 
