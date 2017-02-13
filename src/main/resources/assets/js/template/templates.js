47a1d2bd68c8a73c6608ac7706793b65a0e151a7
david <david.liu.g@outlook.com>
2017-02-13 11:55:30 -0500

    resolve conflict

--- src/main/resources/assets/js/template/templates.js
+++ src/main/resources/assets/js/template/templates.js
@@ -8,13 +8,13 @@ function print() { __p += __j.call(arguments, '') }
 with (obj) {
 
 _.each(books, function (book) {;
-__p += '\r\n<div class="w3-card-4 w3-margin-top">\r\n    <header class="w3-container w3-blue-gray">\r\n        <h3 onclick="window.location.href = \'/book_detail\'; sessionStorage.setItem(\'book_id\', \'' +
+__p += '\r\n<div class="w3-card-4 w3-margin-top">\r\n    <header>\r\n        <ul class="w3-navbar w3-border w3-blue-gray w3-large" style="text-transform: uppercase;">\r\n            <li>\r\n                <a class="w3-hover-blue-gray w3-text-white w3-hover-text-theme" href="/book_detail"\r\n                   onclick="sessionStorage.setItem(\'book_id\', \'' +
 ((__t = (book.id)) == null ? '' : __t) +
-'" class="w3-card-4 w3-margin-top">\r\n    <header>\r\n        <ul class="w3-navbar w3-border w3-blue-gray w3-large" style="text-transform: uppercase;">\r\n            <li>\r\n                <a class="w3-hover-blue-gray w3-text-white w3-hover-text-theme" href="/book_detail">' +
+'\')">' +
 ((__t = (book.title)) == null ? '' : __t) +
 '</a>\r\n            </li>\r\n            <li id="delete_' +
 ((__t = (book.id)) == null ? '' : __t) +
-'" class="w3-right">\r\n                <a class="w3-hover-blue-gray w3-text-white w3-hover-text-theme" href="" onclick="delete_post(\'' +
+'" class="w3-right">\r\n                <a class="w3-hover-blue-gray w3-text-white w3-hover-text-theme" href=""\r\n                   onclick="delete_post(\'' +
 ((__t = (book.id)) == null ? '' : __t) +
 '\');">Delete</a>\r\n            </li>\r\n            <li id="contact_' +
 ((__t = (book.id)) == null ? '' : __t) +
