1f1a1157e2950abfe8973dccc3f728efa509a0f1
david <david.liu.g@outlook.com>
2017-03-01 20:05:15 -0500

    change profile table pid to id, id to user_id

--- source.sql
+++ source.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `booktrader` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `booktrader`;
--- MySQL dump 10.13  Distrib 5.7.12, for osx10.9 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.15, for Win64 (x86_64)
 --
--- Host: localhost    Dbookuseratabase: booktrader
+-- Host: localhost    Database: booktrader
 -- ------------------------------------------------------
--- Server version	5.7.16
+-- Server version	5.7.15-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -32,6 +32,24 @@ CREATE TABLE `book` (
   `price` double NOT NULL,
   `description` varchar(255) NOT NULL,
   `seller` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `profile`
+--
+
+DROP TABLE IF EXISTS `profile`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `profile` (
+  `id` varchar(32) NOT NULL,
+  `about_me` varchar(255) NOT NULL,
+  `rating` decimal(2,1) NOT NULL,
+  `phone_number` varchar(10) NOT NULL,
+  `user_id` varchar(32) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -49,10 +67,11 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL,
   `name` varchar(32) NOT NULL,
   `activation_code` varchar(32) NOT NULL,
-  `status` integer NOT NULL,
+  `status` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`),
-  UNIQUE KEY `name_UNIQUE` (`name`)
+  UNIQUE KEY `name_UNIQUE` (`name`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -65,21 +84,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-DROP TABLE IF EXISTS `profile`;
-
-CREATE TABLE `profile` (
-  `pid` varchar(32) NOT NULL,
-  `about_me` VARCHAR(255) NULL,
-  `rating` DECIMAL(2,1) NULL,
-  `phone_number` VARCHAR(10) NULL,
-  `id` VARCHAR(32) NOT NULL,
-  PRIMARY KEY (`pid`),
-  CONSTRAINT `id`
-    FOREIGN KEY (`id`)
-    REFERENCES `user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
--- Dump completed on 2017-02-11 14:30:43
+-- Dump completed on 2017-03-01 20:03:04
