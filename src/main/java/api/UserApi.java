cbd863969832332728e1d27adb95a5761fb8c872
nevmx <maxsnev@gmail.com>
2017-03-11 16:15:10 -0500

    Added 'oscar' password for test users

--- src/main/java/api/UserApi.java
+++ src/main/java/api/UserApi.java
@@ -66,6 +66,7 @@ public class UserApi {
             if (isATestUser) {
                 user.setStatus(1);
                 user.setEmail(Constant.generateUUID());
+                user.setPassword("oscar");
             } else {
                 user.setStatus(0);
             }
