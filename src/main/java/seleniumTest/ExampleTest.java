1700a597b6bb4db5837b5f0f663dd9aeddc65eef
david <david.liu.g@outlook.com>
2017-03-11 18:26:07 -0500

    add selenium framework and example test.

---
+++ src/main/java/seleniumTest/ExampleTest.java
@@ -0,0 +1,40 @@
+package seleniumTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class ExampleTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 5);
+    }
+
+    @After
+    public void teardown() {
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void test() {
+        driver.navigate().to("https://silentdoor.net");
+        WebElement webElement = driver.findElement(By.cssSelector("body > header > ul > li:nth-child(1) > a"));
+        wait.until(ExpectedConditions.visibilityOf(webElement));
+        String title = webElement.getText();
+        assertEquals(title, "BookTrader");
+    }
+}
